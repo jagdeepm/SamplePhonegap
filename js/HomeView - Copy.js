44e0dbcdd63c561a706326abc2e4b0110ba5cf07
unknown <jmanjunatha@BLRLW3289.asia.manh.com>
2014-08-13 12:50:36 +0530

    Part7

---
+++ js/HomeView - Copy.js
@@ -0,0 +1,34 @@
+var HomeView=function (store) {
+
+  this.intialize = function(){
+  //Define a div wrapper
+  this.el=$('<div/>');
+  this.el.on($('keyup','.search-key'),this.findByName);
+  };
+ this.render = function() {
+    this.el.html(HomeView.template());
+    return this;
+    };
+
+  this.findByName = function() {
+    store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(HomeView.liTemplate(employees));
+    });
+
+
+    };
+	
+  this.intialize();
+
+
+
+
+
+
+
+
+
+}
+
+HomeView.template=Handlebars.compile($("#home-tpl").html());
+HomeView.liTemplate=Handlebars.compile($("#employee-li-tpl").html());
