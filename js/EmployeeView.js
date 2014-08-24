206854f4ff584cb934c90e496bad40e079c59d6a
jagdeepm <jagdeep.m@outlook.com>
2014-08-25 01:19:39 +0530

    Contacts API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,11 +4,11 @@ var EmployeeView = function(employee) {
         this.el = $('<div/>');
 		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
  
   this.render = function() {
-   alert("Hi");
-     this.el.html(EmployeeView.template(employee));
+        this.el.html(EmployeeView.template(employee));
      return this;
     };
   this.addLocation = function(event) {
@@ -21,6 +21,23 @@ var EmployeeView = function(employee) {
         function() {
             alert('Error getting location');
         });
+    return false;
+  };
+  
+  this.addToContacts = function(event) {
+    event.preventDefault();
+    console.log('addToContacts');
+    if (!navigator.contacts) {
+        app.showAlert("Contacts API not supported", "Error");
+        return;
+    }
+    var contact = navigator.contacts.create();
+    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+    var phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+    contact.phoneNumbers = phoneNumbers;
+    contact.save();
     return false;
   };
     this.initialize();
